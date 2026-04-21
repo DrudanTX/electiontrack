28f90181ec112b54dbdc3a88418060b236fb7099
DrudanTX <druvsatya@gmail.com>
2026-04-20 23:39:08 -0700

    Update index.py

--- api/index.py
+++ api/index.py
@@ -5,3 +5,10 @@ app = FastAPI()
 @app.get("/")
 def home():
     return {"status": "working"}
+
+@app.get("/predict")
+def predict():
+    return {
+        "message": "prediction endpoint alive",
+        "note": "we will add model next"
+    }
